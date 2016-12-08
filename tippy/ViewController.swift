2d021b14264e61ebb3632c820d654829ab473539
ALBERTO FRANCO <afranco07@itcs39135n1301.itcs.ccny.lan>
2016-12-08 17:57:19 -0500

    Completed the walkthrough portion of the project. Added responsiveness to the app. The values update as you change the value in the text box and you get the correct tip amount + total displayed.

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +29,18 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
