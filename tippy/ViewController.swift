f0b99177123e6f1e356d1a38c3388739175a0b0d
afranco07 <afranco07@citymail.cuny.edu>
2017-01-06 20:00:49 -0500

    Added the settings menu with some buttons

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -41,6 +41,28 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        //This a good place to retrieve the default tip percentage from UserDefaults
+        //and use i to update the tip amount
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
 
 }
 
